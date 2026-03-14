315a2ae5f679e1a049be0e3304ebfc0753026dfc
ever360 <ever360@users.noreply.github.com>
2026-03-14 16:26:45 +0000

    deploy: 2bce9c6221a482b0a00518a23d5b9aefed572deb

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"e4b8dca3f1b4ede4c30371002441c88c12187e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4058820772" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2646752802" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
